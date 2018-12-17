feb4e720ca6e2216d1ad9d9cbd901babee35c291
hemora <33975301+hemora@users.noreply.github.com>
2018-12-16 20:42:31 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+* Permite imprimir el famoso "Hello World" utilizando el lenguaje Java
+* Java es un lenguaje de programacion agrupado dentro del  paradigma orientado a objetos
+*/
+public class HelloWorld {
+  public static void main (String[]args) {
+    System.out.println("Hello World") ;
+  } //END main
+} // END HelloWorld 
